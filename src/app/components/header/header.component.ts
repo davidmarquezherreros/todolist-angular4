2a37e683e15585c7348b8ebc445ea41901fa6d05
David Marquez Herreros <davidmarquezherreros@gmail.com>
2018-02-11 18:45:53 +0100

    Component header created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '../../models/constants.model';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public title: string;
+  public todos: string;
+  public createTodo: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.title = Constants.Views.NameApp;
+    this.todos = Constants.Views.ToDos;
+    this.createTodo = Constants.Views.CreateToDo;
   }
 
 }
