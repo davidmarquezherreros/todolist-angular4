4b1ba06693d948dabd3e7044fccd36e0212225b1
David Marquez Herreros <davidmarquezherreros@gmail.com>
2018-02-11 18:45:35 +0100

    Constants model added

---
+++ src/app/models/constants.model.ts
@@ -0,0 +1,8 @@
+export class Constants {
+    static Views = {
+        NameApp: 'ToDoList',
+        ToDos: 'List of ToDos',
+        CreateToDo: 'Create a ToDo',
+        UpdateToDo: 'Update ToDo'
+    };
+}
